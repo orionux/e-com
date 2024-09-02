15992ba06b7429924b743f2c2acdfec24172c67b
DelaBandara <kanishkadela@gmail.com>
2024-09-02 13:47:44 +0530

    sigin page update

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -3,10 +3,10 @@ import styles from '../../styles/signIn/signIn.module.css';
 
 const SignIn = () => {
   return (
-    <div className={styles.registerWrapper}>
-      <div className={styles.registerSection}>
+    <div className={styles.signinWrapper}>
+      <div className={styles.signinSection}>
         
-        <div className={styles.registerForm}>
+        <div className={styles.signinForm}>
           <form action="#" method="post">
             <h2>Log In</h2>
             <h5>To connect with us bla bla bla</h5>
@@ -14,13 +14,13 @@ const SignIn = () => {
               <input type="text" name="user-name" placeholder="User Name" />
             </div>
 
-            <div className={styles.registerPasswords}>
+            <div className={styles.signinPasswords}>
               <div className={styles.Password}>
                 <input type="password" name="user-password" placeholder="Password"/>
               </div>
             </div>
 
-            <div className={styles.regBtn}>
+            <div className={styles.signinBtn}>
               <a href='#'><u>Forget Password?</u></a>
               <button type="submit">LogIn</button>
             </div>
@@ -29,10 +29,10 @@ const SignIn = () => {
           </form>
         </div>
       </div>
-      <div className={styles.regImg}>
+      <div className={styles.signinImg}>
       <h1>Original Products</h1>
       <p>All types of tobacco and related products with a guarantee. </p>
-        <img src="/images/signIn/signIn.png" alt="" width='900px' className=''/>
+        <img src="/images/signIn/signIn.png" alt="" width='800px' className=''/>
       </div>
     </div>
   )
