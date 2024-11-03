909bebf030335f741abbcaba3471874ae5784bf5
tj <thinupavani@gmail.com>
2024-11-03 14:04:25 +0530

    update

--- Components/Footer/index.tsx
+++ Components/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="footer-top-area pt-70 pb-35 wrapper-padding-5">
         <div className="container-fluid">
           <div className="widget-wrapper">
-            <div className="footer-widget mb-30 col-12 col-lg-6">
+            <div className="footer-widget mb-30 col-12 col-lg-5">
               <a href="#">
                 <img
                   src="/assets/img/logo.png"
@@ -17,11 +17,54 @@ const Footer = () => {
               </a>
               <div className="footer-about-2">
                 <p style={{ maxWidth: "70%" }}>
-                Fort House is Sri Lanka’s leading website for premium smoking accessories, offering top-quality products such as rolling papers, lighters, and unique smoking tools. Known for its unbeatable offers and promotions, Fort House provides a wide range of items to meet every preference, delivering exceptional value and variety to customers across the island.
+                  Fort House is Sri Lanka’s leading website for premium smoking
+                  accessories, offering top-quality products such as rolling
+                  papers, lighters, and unique smoking tools. Known for its
+                  unbeatable offers and promotions, Fort House provides a wide
+                  range of items to meet every preference, delivering
+                  exceptional value and variety to customers across the island.
                 </p>
               </div>
             </div>
-            <div className="footer-widget mb-30 col-12 col-lg-6">
+            <div className="footer-widget mb-30 col-12 col-lg-3 d-flex flex-column flex-lg-row">
+              <div className="d-flex  flex-column col-12 col-lg-6">
+                <h3 className="footer-widget-title-5">Shop</h3>
+                <div className="footer-info-wrapper-3">
+                  <div className="footer-address-furniture">
+                    <div className="footer-info-content3 d-flex flex-column">
+                      <a href="/product" className="mb-3">
+                        All products
+                      </a>
+                      <a href="/product" className="mb-3">
+                        Popular
+                      </a>
+                      <a href="/product" className="mb-3">
+                        Categories
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="d-flex flex-column col-12 col-lg-6">
+              <h3 className="footer-widget-title-5">Help</h3>
+              <div className="footer-info-wrapper-3">
+                <div className="footer-address-furniture">
+                  <div className="footer-info-content3 d-flex flex-column">
+                    <a href="/product" className="mb-3">
+                      Shipping
+                    </a>
+                    <a href="/product" className="mb-3">
+                      Refunding
+                    </a>
+                    <a href="/product" className="mb-3">
+                      Terms & Conditions
+                    </a>
+                  </div>
+                </div>
+              </div>
+              </div>
+            </div>
+            <div className="footer-widget mb-30 col-12 col-lg-4 ps-lg-5">
               <h3 className="footer-widget-title-5">Contact Info</h3>
               <div className="footer-info-wrapper-3">
                 <div className="footer-address-furniture">
@@ -51,7 +94,10 @@ const Footer = () => {
                   </div>
                   <div className="footer-info-content3">
                     <p>
-                      <a href="mailto:  info@forthouse.com"> info@forthouse.com</a>{" "}
+                      <a href="mailto:  info@forthouse.com">
+                        {" "}
+                        info@forthouse.com
+                      </a>{" "}
                     </p>
                   </div>
                 </div>
@@ -66,8 +112,8 @@ const Footer = () => {
             <div className="col-12 text-center">
               <div className="copyright-furniture">
                 <p>
-                  Copyright © 2024 <a href="/">Fort house</a>. All Right Reserved -
-                  Developed By ORION UX
+                  Copyright © 2024 <a href="/">Fort house</a>. All Right
+                  Reserved - Developed By ORION UX
                 </p>
               </div>
             </div>
